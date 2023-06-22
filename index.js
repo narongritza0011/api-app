42deedd58c25346af699a09b92280c21d5f4ef99
narongrit <narongritza0011@gmail.com>
2023-06-22 20:27:59 +0700

    test

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import router from "./routes/index.js";
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Choo Choo! Welcome to your Express app 🚅");
-// });
+ app.get("/", (req, res) => {
+   res.send("Choo Choo! Welcome to your Express app 🚅");
+ });
 
 // app.get("/json", (req, res) => {
 //   res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(router);
+// app.use(router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
